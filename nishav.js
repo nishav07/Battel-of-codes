6ca76eb8edb62ea26b3ddd0bb8d9f5bf1f938434
unknown <nishavkumar07@gmail.com>
2025-06-19 16:24:04 +0530

    practiced callback hell

--- nishav.js
+++ nishav.js
@@ -1,2 +1,19 @@
 const h1 = document.querySelector("#h");
-console.log(h1);
+
+
+function changecolor(color,time,next){
+    setTimeout(() => {
+        h1.style.color = color;
+        if(next) next();
+    },time);
+}
+
+changecolor("blue" , 2000 , () => {
+    changecolor("green" , 2000 , () => {
+        changecolor("pink" , 2000, () => {
+            changecolor("royalblue" , 2000 , () => {
+                changecolor("red" , 2000 )
+            })
+        })
+    })
+})
