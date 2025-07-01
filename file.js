6bd8a3098c503c9f669721f2cdca002cfa5e3949
unknown <nishavkumar07@gmail.com>
2025-07-01 18:08:50 +0530

    ello

--- file.js
+++ file.js
@@ -1,10 +1,16 @@
-const { readFile } = require('fs');
-const fs = require('fs/promises');
+// const { readFile } = require('fs');
+// const fs = require('fs/promises');
 
-async function showdata() {
-    await fs.writeFile('me.txt' , "hello my name is nishav chaudhary")
-    let data = await fs.readFile("me.txt" , "utf-8")
-    console.log(data)
+// async function showdata() {
+//     await fs.writeFile('me.txt' , "hello my name is nishav chaudhary")
+//     let data = await fs.readFile("me.txt" , "utf-8")
+//     console.log(data)
+// }
+
+// showdata()
+
+
+function coolors async(theme){
+    const res =  fetch(`https://colormagic.app/api/palette/search?q=${theme}`);
+    const data = await res.json();
 }
-
-showdata()
