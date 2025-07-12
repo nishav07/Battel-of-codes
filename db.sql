c5e7ed7d9b9f3f9a4d548d9d1399c4d68561f897
unknown <nishavkumar07@gmail.com>
2025-07-12 14:44:35 +0530

    inserting data in db table

--- db.sql
+++ db.sql
@@ -1,7 +1,17 @@
-CREATE DATABASE Users;
+CREATE DATABASE IF NOT EXISTS Users;
 USE Users;
 
 CREATE TABLE detail (
-    userid INT PRIMARY KEY;
-    
+    userid INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    age INT CHECK(age > 18),
+    city VARCHAR(20) NOT NULL;
 );
+
+INSERT INTO detail
+(name,age,city,userid)
+VALUES
+("Rohan",19,"pune",12),
+("Rohan",19,"pune",12),
+("Rohan",19,"pune",12),
+("Rohan",19,"pune",12),
